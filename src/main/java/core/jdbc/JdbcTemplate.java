46abf084b27c069127e68bb9fc0a4042537c9357
Young Jun Park <june0313@gmail.com>
2018-05-29 00:53:54 +0900

    SQL 인자를 가변인자로 받을 수 있도록 JdbcTemplate 메소드 수정

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -8,45 +8,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
-	public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
-		try (Connection con = ConnectionManager.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-			pss.setValues(ps);
+	public void update(String sql, Object... parameters) throws DataAccessException {
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {	
+			setValues(ps, parameters);
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			throw new DataAccessException(e);
 		}
 	}
+
+	public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object...parameters) throws DataAccessException {
+		List<T> result = query(sql, rowMapper, parameters);
+
+		if (result.isEmpty()) {
+			return null;
+		}
+
+		return result.get(0);
+	}
 	
-	public <T> List<T> query(String sql, PreparedStatementSetter pss, RowMapper<T> rowMapper) throws DataAccessException {
+	public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object...parameters) throws DataAccessException {
 		try (Connection con = ConnectionManager.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-			pss.setValues(ps);
+			setValues(ps, parameters);		
 			return getResult(ps, rowMapper);
 		} catch (SQLException e) {
 			throw new DataAccessException(e);
 		}
 	}
-	
-	public <T> T queryForObject(String sql, PreparedStatementSetter pss, RowMapper<T> rowMapper) throws DataAccessException {
-		List<T> result = query(sql, pss, rowMapper);
-		
-		if (result.isEmpty()) {
-			return null;
-		}
-		
-		return result.get(0);
-	}
 
 	private <T> List<T> getResult(PreparedStatement ps, RowMapper<T> rowMapper) throws DataAccessException {
 		try (ResultSet rs = ps.executeQuery()) {
 			List<T> result = new ArrayList<>();
-			
+
 			while (rs.next()) {
 				result.add(rowMapper.mapRow(rs));
 			}
-			
+
 			return result;
 		} catch (SQLException e) {
 			throw new DataAccessException(e);
+		}
+	}
+	
+	private void setValues(PreparedStatement ps, Object... parameters) throws SQLException {
+		for (int i = 0; i < parameters.length; i++) {
+			ps.setObject(i + 1,  parameters[i]);
 		}
 	}
 }
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -3,56 +3,39 @@ package next.dao;
 import java.util.List;
 
 import core.jdbc.JdbcTemplate;
-import core.jdbc.PreparedStatementSetter;
 import core.jdbc.RowMapper;
 import next.model.User;
 
 public class UserDao {
 	public void insert(User user) {
 		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-		PreparedStatementSetter pss = ps -> {
-			ps.setString(1, user.getUserId());
-			ps.setString(2, user.getPassword());
-			ps.setString(3, user.getName());
-			ps.setString(4, user.getEmail());
-		};
-
-		new JdbcTemplate().update(sql, pss);
+		new JdbcTemplate().update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
 	}
 
 	public void update(User user) {
 		String sql = "UPDATE users SET password=?, name=?, email=? WHERE userId=?";
-		PreparedStatementSetter pss = ps -> {
-			ps.setString(1, user.getPassword());
-			ps.setString(2, user.getName());
-			ps.setString(3, user.getEmail());
-			ps.setString(4, user.getUserId());
-		};
-		
-		new JdbcTemplate().update(sql, pss);
+		new JdbcTemplate().update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
 	}
 
 	public List<User> findAll() {
 		String sql = "SELECT userId, password, name, email FROM USERS";
-		PreparedStatementSetter pss = ps -> {};
 		RowMapper<User> rowMapper = rs -> new User(
 				rs.getString("userId"), 
 				rs.getString("password"), 
 				rs.getString("name"), 
 				rs.getString("email"));
 		
-		return new JdbcTemplate().query(sql, pss, rowMapper);
+		return new JdbcTemplate().query(sql, rowMapper);
 	}
 
 	public User findByUserId(String userId) {
 		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-		PreparedStatementSetter pss = ps -> ps.setString(1, userId);
 		RowMapper<User> rowMapper = rs -> new User(
 				rs.getString("userId"), 
 				rs.getString("password"), 
 				rs.getString("name"), 
 				rs.getString("email"));
 		
-		return new JdbcTemplate().queryForObject(sql, pss, rowMapper);
+		return new JdbcTemplate().queryForObject(sql, rowMapper, userId);
 	}
 }
